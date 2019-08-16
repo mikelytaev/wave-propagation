0b4c73a4c80f5b5a44175a765772283ea85c868d
mikhail <mikelytaev@gmail.com>
2019-08-16 18:22:30 +0300

    ts example added

---
+++ examples/thin_scattering/ex1.py
@@ -0,0 +1,26 @@
+from propagators.ts import *
+from rwp.field import *
+from rwp.vis import *
+
+
+comp_params = ThinScatteringComputationalParams(max_p_k0=1.001,
+                                                p_grid_size=250,
+                                                quadrature_points=4,
+                                                alpha=1e-5,
+                                                spectral_integration_method=SpectralIntegrationMethod.fcc,
+                                                x_grid_m=np.linspace(-5, 5, 500),
+                                                z_grid_m=np.linspace(-5, 5, 500))
+bodies = []
+bodies += [Plate(x0_m=-2.5, z1_m=-2.5, z2_m=3.5, width_m=0.1, eps_r=50)]
+#bodies += [Plate(x0_m=5, z1_m=-7.5, z2_m=5, width_m=0.1, eps_r=50)]
+ts = ThinScattering(wavelength=1, bodies=bodies, params=comp_params, save_debug=False)
+f = ts.calculate()
+
+field = Field(x_grid=ts.x_computational_grid, z_grid=ts.z_computational_grid, freq_hz=300e6)
+field.field[:, :] = f[:, :]
+vis = FieldVisualiser(field, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)), label='ke')
+plt = vis.plot2d(min=-40, max=0)
+plt.title('The intensity of the field component 10log10|u|')
+plt.xlabel('Range (m)')
+plt.ylabel('Height (m)')
+plt.show()
