db0924afd4d497cd604eaa05d82df6d0b6bf9c97
Mikhail <mikelytaev@gmail.com>
2020-08-14 00:17:55 +0300

    [*] еще эксперименты с 4th order

--- examples/optimization/tap_numerov_pade2.py
+++ examples/optimization/tap_numerov_pade2.py
@@ -1,33 +1,22 @@
 import numpy as np
 import cmath as cm
-from scipy.interpolate import interp2d
+import math as fm
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
-import pyproj
 
 
-bounds = (29.636637, 60.112502, 31.288706, 60.699130)
-west, south, east, north = bounds
+def elev_int_1d(x, angle, height, r):
+    length = height / fm.tan(angle * cm.pi / 180)
+    if r <= x <= r + length:
+        return (x - r) * fm.tan(angle * cm.pi / 180)
+    elif r + length < x <= r + 2*length:
+        return (r + 2*length - x) * fm.tan(angle * cm.pi / 180)
+    else:
+        return 0
 
-import pickle
-
-with open('elevation.pickle', 'rb') as f:
-    elevation = pickle.load(f)
-
-geod = pyproj.Geod(ellps='WGS84')
-
-azimuth1, azimuth2, distance = geod.inv(west, south, east, north)
-
-long_grid = np.linspace(west, east, elevation.shape[0])
-lat_grid = np.linspace(north, south, elevation.shape[1])
-
-elev_int = interp2d(long_grid, lat_grid, elevation.T)
-
-def elev_int_1d(x):
-    if not 0 < x < distance:
-        return 0.0
-    v = x / distance
-    return max(elev_int(west + (east - west) * v, south + (north - south) * v)[0], 0)
+# x_grid = np.linspace(0, 20000, 1000)
+# plt.plot(x_grid, [elev_int_1d(v) for v in x_grid])
+# plt.show()
 
 
 from rwp.sspade import *
@@ -36,15 +25,15 @@ from rwp.vis import *
 
 logging.basicConfig(level=logging.DEBUG)
 env = Troposphere()
-env.z_max = 5000
-env.terrain = Terrain(elevation=elev_int_1d, ground_material=FreshWater())
+env.z_max = 300
+env.terrain = Terrain(elevation=lambda x: elev_int_1d(x, 5, 200, 7e3) + elev_int_1d(x, 5, 200, 14e3), ground_material=PerfectlyElectricConducting())
 #env.knife_edges = [KnifeEdge(80e3, 200)]
 profile1d = interp1d(x=[0, 100, 150, 300], y=[0, 32, 10, 40], fill_value="extrapolate")
 #env.M_profile = lambda x, z: profile1d(z)
 
 ant = GaussAntenna(freq_hz=3000e6, height=70, beam_width=4, eval_angle=0, polarz='H')
-
-pade_task_4 = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=150e3, comp_params=
+max_range_m = 20000
+pade_task_4 = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=max_range_m, comp_params=
                                                   HelmholtzPropagatorComputationalParams(
                                                       terrain_method=TerrainMethod.staircase,
                                                       max_propagation_angle=5,
@@ -53,26 +42,26 @@ pade_task_4 = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_ra
                                                       z_order=4,
                                                       exp_pade_order=(10, 11),
                                                       dx_wl=500,
-                                                      dz_wl=0.8,
+                                                      dz_wl=1.4,
                                                       #two_way=False,
                                                       storage=PickleStorage()
                                                   ))
 pade_field_4 = pade_task_4.calculate()
 
-pade_task_4f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=150e3, comp_params=
-                                                  HelmholtzPropagatorComputationalParams(
-                                                      terrain_method=TerrainMethod.staircase,
-                                                      max_propagation_angle=5,
-                                                      #modify_grid=True,
-                                                      grid_optimizator_abs_threshold=5e-3,
-                                                      z_order=4,
-                                                      exp_pade_order=(10, 11),
-                                                      dx_wl=500,
-                                                      dz_wl=0.8*4.5,
-                                                      #two_way=False,
-                                                      storage=PickleStorage()
-                                                  ))
-pade_field_4f = pade_task_4f.calculate()
+# pade_task_4f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=max_range_m, comp_params=
+#                                                   HelmholtzPropagatorComputationalParams(
+#                                                       terrain_method=TerrainMethod.staircase,
+#                                                       max_propagation_angle=5,
+#                                                       #modify_grid=True,
+#                                                       grid_optimizator_abs_threshold=5e-3,
+#                                                       z_order=4,
+#                                                       exp_pade_order=(10, 11),
+#                                                       dx_wl=500,
+#                                                       dz_wl=0.8*4.5,
+#                                                       #two_way=False,
+#                                                       storage=PickleStorage()
+#                                                   ))
+# pade_field_4f = pade_task_4f.calculate()
 
 # pade_task_2 = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=150e3, comp_params=
 #                                                   HelmholtzPropagatorComputationalParams(
@@ -88,7 +77,7 @@ pade_field_4f = pade_task_4f.calculate()
 #                                                   ))
 # pade_field_2 = pade_task_2.calculate()
 
-pade_task_2f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=150e3, comp_params=
+pade_task_2f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=max_range_m, comp_params=
                                                   HelmholtzPropagatorComputationalParams(
                                                       terrain_method=TerrainMethod.staircase,
                                                       max_propagation_angle=5,
@@ -97,7 +86,7 @@ pade_task_2f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_r
                                                       z_order=2,
                                                       exp_pade_order=(10, 11),
                                                       dx_wl=500,
-                                                      dz_wl=0.8*4.5,
+                                                      dz_wl=1.4,
                                                       #two_way=False,
                                                       storage=PickleStorage()
                                                   ))
@@ -114,8 +103,8 @@ pade_field_2f = pade_task_2f.calculate()
 
 pade_vis_4 = FieldVisualiser(pade_field_4, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
                              label='Pade-[10/11], dx=500, dz=0.8 (4th order)', x_mult=1E-3)
-pade_vis_4f = FieldVisualiser(pade_field_4f, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
-                             label='Pade-[10/11], dx=500, dz=3.6 (4th order)', x_mult=1E-3)
+# pade_vis_4f = FieldVisualiser(pade_field_4f, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
+#                              label='Pade-[10/11], dx=500, dz=3.6 (4th order)', x_mult=1E-3)
 # pade_vis_2 = FieldVisualiser(pade_field_2, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
 #                              label='Pade-[10/11], dx=500, dz=0.1 (2th order)', x_mult=1E-3)
 pade_vis_2f = FieldVisualiser(pade_field_2f, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
@@ -129,11 +118,11 @@ plt.ylabel('Height (m)')
 plt.tight_layout()
 plt.show()
 
-plt = pade_vis_4f.plot2d(min=-100, max=0, show_terrain=True)
-plt.xlabel('Range (km)')
-plt.ylabel('Height (m)')
-plt.tight_layout()
-plt.show()
+# plt = pade_vis_4f.plot2d(min=-100, max=0, show_terrain=True)
+# plt.xlabel('Range (km)')
+# plt.ylabel('Height (m)')
+# plt.tight_layout()
+# plt.show()
 
 # plt = pade_vis_2.plot2d(min=-100, max=0, show_terrain=True)
 # plt.xlabel('Range (km)')
@@ -160,17 +149,17 @@ plt.show()
 # plt.tight_layout()
 # plt.show()
 
-plt = pade_vis_4.plot_hor_over_terrain(10.79913607, pade_vis_2f, pade_vis_4f)
+plt = pade_vis_4.plot_hor_over_terrain(1, pade_vis_2f)
 plt.xlabel('Range (km)')
 plt.ylabel('10log|u| (dB)')
-plt.xlim([0.5, 150])
+plt.xlim([0.5, 20])
 plt.ylim([-100, 0])
 plt.grid(True)
 plt.tight_layout()
 plt.show()
 
 f, (ax1) = plt.subplots(1, 1, sharey=True)
-pade_vis_4.plot_ver(5 * 1E3, ax1, pade_vis_2f, pade_vis_4f)
+pade_vis_4.plot_ver(5 * 1E3, ax1, pade_vis_2f)
 ax1.set_ylabel('Height (m)')
 ax1.set_xlabel('10log|u| (dB)')
 ax1.grid()
@@ -178,29 +167,9 @@ f.tight_layout()
 f.show()
 
 f, (ax1) = plt.subplots(1, 1, sharey=True)
-pade_vis_4.plot_ver(150 * 1E3, ax1, pade_vis_2f, pade_vis_4f)
+pade_vis_4.plot_ver(150 * 1E3, ax1, pade_vis_2f)
 ax1.set_ylabel('Height (m)')
 ax1.set_xlabel('10log|u| (dB)')
 ax1.grid()
 f.tight_layout()
-f.show()
-
-# pade_vis_4.plot_ver(150 * 1E3, ax2, pade_vis_2f, pade_vis_4f)
-# ax2.set_ylabel('Height (m)')
-# ax2.set_xlabel('10log|u| (dB)')
-
-
-f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-ax1.plot([a / 6371000 * 1E6 for a in pade_field_4.z_grid], pade_field_4.z_grid)
-#ax1.legend()
-ax1.set_xlabel('M-units')
-ax1.set_ylabel('Height (m)')
-
-ax2.plot([profile1d(a) for a in pade_field_4.z_grid], pade_field_4.z_grid)
-#ax2.legend()
-ax2.set_xlabel('M-units')
-ax2.set_ylabel('Height (m)')
-f.tight_layout()
-ax1.grid()
-ax2.grid()
 f.show()
