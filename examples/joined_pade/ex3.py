75ef1b0b91c4490dfa2e28f5f5d9773a053009cf
mikhail <mikelytaev@gmail.com>
2020-08-26 17:04:00 +0300

    [+] duct + pyramid example

--- examples/joined_pade/ex3.py
+++ examples/joined_pade/ex3.py
@@ -6,14 +6,17 @@ from rwp.vis import *
 logging.basicConfig(level=logging.DEBUG)
 env = Troposphere(flat=False)
 env.z_max = 300
-env.terrain = Terrain(ground_material=PerfectlyElectricConducting())
+env.terrain = Terrain(elevation=lambda x: pyramid(x, 5, 50, 50e3) +
+                                          pyramid(x, 5, 100, 70e3) +
+                                          pyramid(x, 5, 150, 90e3),
+                      ground_material=PerfectlyElectricConducting())
 profile1d = interp1d(x=[0, 100, 150, 300], y=[0, 32, 10, 50], fill_value="extrapolate")
 env.M_profile = lambda x, z: profile1d(z)
 
 ant = GaussAntenna(freq_hz=3000e6, height=150, beam_width=4, eval_angle=0, polarz='H')
 
 max_propagation_angle = 3
-max_range_m = 200e3
+max_range_m = 100e3
 
 pade_task_2f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=max_range_m, comp_params=
                                                   HelmholtzPropagatorComputationalParams(
@@ -23,8 +26,9 @@ pade_task_2f = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_r
                                                       grid_optimizator_abs_threshold=5e-3,
                                                       z_order=2,
                                                       exp_pade_order=(10, 11),
-                                                      dx_wl=1000,
-                                                      dz_wl=0.5,
+                                                      dx_wl=500,
+                                                      dz_wl=1,
+                                                      z_output_filter=3,
                                                       storage=PickleStorage()
                                                   ))
 pade_field_2f = pade_task_2f.calculate()
@@ -37,8 +41,8 @@ pade_task_joined = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, m
                                                       grid_optimizator_abs_threshold=5e-3,
                                                       z_order=5,
                                                       exp_pade_order=(10, 11),
-                                                      dx_wl=1000,
-                                                      dz_wl=0.5,
+                                                      dx_wl=500,
+                                                      dz_wl=3,
                                                       storage=PickleStorage()
                                                   ))
 pade_field_joined = pade_task_joined.calculate()
@@ -48,15 +52,31 @@ pade_vis_2f = FieldVisualiser(pade_field_2f, env=env, trans_func=lambda v: 10 * 
 pade_vis_joined = FieldVisualiser(pade_field_joined, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
                              label='Pade-[10/11], dx=500, dz=2.0 (Pade)', x_mult=1E-3)
 
-plt = pade_vis_2f.plot2d(min=-50, max=0, show_terrain=True)
+plt = pade_vis_2f.plot2d(min=-100, max=0, show_terrain=True)
 plt.xlabel('Range (km)')
 plt.ylabel('Height (m)')
 plt.tight_layout()
 plt.show()
 
-plt = pade_vis_joined.plot2d(min=-50, max=0, show_terrain=True)
+plt = pade_vis_joined.plot2d(min=-100, max=0, show_terrain=True)
 plt.xlabel('Range (km)')
 plt.ylabel('Height (m)')
+plt.tight_layout()
+plt.show()
+
+plt = pade_vis_2f.plot2d(min=-100, max=0, show_terrain=True)
+plt.xlabel('Range (km)')
+plt.ylabel('Height (m)')
+plt.ylim([0, 150])
+plt.xlim([65, 80])
+plt.tight_layout()
+plt.show()
+
+plt = pade_vis_joined.plot2d(min=-100, max=0, show_terrain=True)
+plt.xlabel('Range (km)')
+plt.ylabel('Height (m)')
+plt.ylim([0, 150])
+plt.xlim([65, 80])
 plt.tight_layout()
 plt.show()
 
@@ -70,7 +90,7 @@ plt.tight_layout()
 plt.show()
 
 f, (ax1) = plt.subplots(1, 1, sharey=True)
-pade_vis_2f.plot_ver(5.2 * 1E3, ax1, pade_vis_joined)
+pade_vis_2f.plot_ver(72.5 * 1E3, ax1, pade_vis_joined)
 ax1.set_ylabel('Height (m)')
 ax1.set_xlabel('10log|u| (dB)')
 ax1.grid()
