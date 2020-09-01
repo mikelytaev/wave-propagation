f7401aeb89f12d3ec20537d218f1eb00829850b5
Mikhail <mikelytaev@gmail.com>
2020-09-01 23:29:20 +0300

    [*] joint Pade vs petool examples

--- examples/joined_pade/ex1sh.py
+++ examples/joined_pade/ex1sh.py
@@ -1,7 +1,7 @@
 from rwp.sspade import *
 from rwp.vis import *
 from rwp.tworay import *
-#from rwp.petool import PETOOLPropagationTask
+from rwp.petool import PETOOLPropagationTask
 from scipy.io import loadmat
 
 
@@ -89,6 +89,12 @@ pade_task_joined = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, m
                                                   ))
 pade_field_joined = pade_task_joined.calculate()
 
+petool_task_4 = PETOOLPropagationTask(antenna=ant, env=env, two_way=False, max_range_m=max_range_m, dx_wl=100, dz_wl=4)
+petool_field_4 = petool_task_4.calculate()
+
+petool_task_2 = PETOOLPropagationTask(antenna=ant, env=env, two_way=False, max_range_m=max_range_m, dx_wl=100, dz_wl=2)
+petool_field_2 = petool_task_2.calculate()
+
 trm = TwoRayModel(src=ant, env=env)
 x_grid_m = pade_field_joined.x_grid
 z_grid_m = pade_field_joined.z_grid
@@ -108,8 +114,10 @@ pade_vis_joined = FieldVisualiser(pade_field_joined, env=env, trans_func=lambda 
                              label='Pade-[10/11], dx=500, dz=2.0 (Pade)', x_mult=1E-3)
 trm_vis = FieldVisualiser(trm_field, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v))-10,
                           label='Two-ray model', x_mult=1E-3)
+petool_vis_4 = FieldVisualiser(petool_field_4, env=env, trans_func=lambda x: x, label='Метод расщепления Фурье', x_mult=1E-3)
+petool_vis_2 = FieldVisualiser(petool_field_2, env=env, trans_func=lambda x: x, label='Метод расщепления Фурье', x_mult=1E-3)
 
-f, ax = plt.subplots(3, 2, sharey=True, figsize=(6, 2.0*3), constrained_layout=True)
+f, ax = plt.subplots(4, 2, sharey=True, figsize=(6, 2.0*4), constrained_layout=True)
 norm = Normalize(-50, 0)
 extent = [pade_vis_joined.x_grid[0], pade_vis_joined.x_grid[-1], pade_vis_joined.z_grid[0], pade_vis_joined.z_grid[-1]]
 ax[0][0].imshow(trm_vis.field.T[::-1, :], extent=extent, norm=norm, aspect='auto', cmap=plt.get_cmap('jet'))
@@ -136,15 +144,25 @@ ax[1][1].set_title('(г)')
 ax[2][0].imshow(pade_vis_2_1.field.T[::-1, :], extent=extent, norm=norm, aspect='auto', cmap=plt.get_cmap('jet'))
 ax[2][0].grid()
 ax[2][0].set_title('(д)')
-ax[2][0].set_xlabel('Расстояние, км')
 ax[2][0].set_ylabel('Высота, м')
 
 im = ax[2][1].imshow(pade_vis_2_2.field.T[::-1, :], extent=extent, norm=norm, aspect='auto', cmap=plt.get_cmap('jet'))
 ax[2][1].grid()
 ax[2][1].set_title('(е)')
-ax[2][1].set_xlabel('Расстояние, км')
 #ax[0][1].set_ylabel('Высота, м')
 
-f.colorbar(im, ax=ax[2, :], shrink=0.6, location='bottom')
+ax[3][0].imshow(petool_vis_4.field.T[::-1, :], extent=extent, norm=norm, aspect='auto', cmap=plt.get_cmap('jet'))
+ax[3][0].grid()
+ax[3][0].set_title('(ж)')
+ax[3][0].set_xlabel('Расстояние, км')
+ax[3][0].set_ylabel('Высота, м')
+
+ax[3][1].imshow(petool_vis_2.field.T[::-1, :], extent=extent, norm=norm, aspect='auto', cmap=plt.get_cmap('jet'))
+ax[3][1].grid()
+ax[3][1].set_title('(з)')
+ax[3][1].set_xlabel('Расстояние, км')
+#ax[0][1].set_ylabel('Высота, м')
+
+f.colorbar(im, ax=ax[3, :], shrink=0.6, location='bottom')
 #f.tight_layout()
 plt.show()
