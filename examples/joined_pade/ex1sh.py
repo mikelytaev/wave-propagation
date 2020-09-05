80c1c38ea5aaed04ef21b5fe56d099316929c1b0
Mikhail <mikelytaev@gmail.com>
2020-09-05 23:11:23 +0300

    [*] cn examples

--- examples/joined_pade/ex1sh.py
+++ examples/joined_pade/ex1sh.py
@@ -89,6 +89,21 @@ pade_task_joined = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, m
                                                   ))
 pade_field_joined = pade_task_joined.calculate()
 
+pade_task_cn = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=max_range_m, comp_params=
+                                                  HelmholtzPropagatorComputationalParams(
+                                                      terrain_method=TerrainMethod.staircase,
+                                                      max_propagation_angle=max_propagation_angle,
+                                                      modify_grid=False,
+                                                      grid_optimizator_abs_threshold=5e-3,
+                                                      z_order=2,
+                                                      exp_pade_order=(1, 1),
+                                                      dx_wl=100/40,
+                                                      x_output_filter=40,
+                                                      dz_wl=0.25,
+                                                      storage=PickleStorage()
+                                                  ))
+pade_field_cn = pade_task_cn.calculate()
+
 petool_task_4 = PETOOLPropagationTask(antenna=ant, env=env, two_way=False, max_range_m=max_range_m, dx_wl=100, dz_wl=4)
 petool_field_4 = petool_task_4.calculate()
 
@@ -111,6 +126,8 @@ pade_vis_2_1 = FieldVisualiser(pade_field_2_1, env=env, trans_func=lambda v: 10 
 pade_vis_2_2 = FieldVisualiser(pade_field_2_2, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
                              label='Pade-[10/11], dx=100, dz=2.0 (2nd order)', x_mult=1E-3)
 pade_vis_joined = FieldVisualiser(pade_field_joined, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
+                             label='Pade-[10/11], dx=500, dz=2.0 (Pade)', x_mult=1E-3)
+pade_vis_cn = FieldVisualiser(pade_field_cn, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)),
                              label='Pade-[10/11], dx=500, dz=2.0 (Pade)', x_mult=1E-3)
 trm_vis = FieldVisualiser(trm_field, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v))-10,
                           label='Two-ray model', x_mult=1E-3)
@@ -165,4 +182,10 @@ ax[3][1].set_xlabel('Расстояние, км')
 
 f.colorbar(im, ax=ax[3, :], shrink=0.6, location='bottom')
 #f.tight_layout()
+plt.show()
+
+plt = pade_vis_cn.plot2d(min=-50, max=0, show_terrain=True)
+plt.xlabel('Range (km)')
+plt.ylabel('Height (m)')
+plt.tight_layout()
 plt.show()
