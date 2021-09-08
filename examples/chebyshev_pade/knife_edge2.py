d53d86b76d9b80899a7a6065a21ef0de4e261277
Mikhail Lytaev <mikelytaev@gmail.com>
2021-09-09 01:59:05 +0300

    Cheb.-Pade knife-edge

--- examples/chebyshev_pade/knife_edge2.py
+++ examples/chebyshev_pade/knife_edge2.py
@@ -6,27 +6,31 @@ from cheb_pade_coefs import *
 
 logging.basicConfig(level=logging.DEBUG)
 
-dx_wl = 6
-coefs, a0 = cheb_pade_coefs(dx_wl, (9, 9), fm.sin(89*fm.pi/180)**2, 'aaa')
+dx_wl = 5
+max_propagation_angle = 85
+print(max_propagation_angle)
+coefs, a0 = cheb_pade_coefs(dx_wl, (10, 10), fm.sin(max_propagation_angle*fm.pi/180)**2, 'ratinterp')
 
 env = Troposphere(flat=True)
-env.z_max = 300
-env.knife_edges = [KnifeEdge(range=0.5e3, height=150)]
+env.z_max = 100
+env.knife_edges = [KnifeEdge(range=0.1e3, height=10),
+                   KnifeEdge(range=0.25e3, height=50),
+                   KnifeEdge(range=0.40e3, height=75)]
 
-ant = GaussAntenna(freq_hz=3000e6, height=150, beam_width=20, eval_angle=0, polarz='H')
+ant = GaussAntenna(freq_hz=3000e6, height=50, beam_width=20, eval_angle=0, polarz='H')
 
-max_range_m = 1e3
+max_range_m = 0.5e3
 
 cheb_pade_task = TroposphericRadioWaveSSPadePropagator(antenna=ant, env=env, max_range_m=max_range_m, comp_params=
                                                   HelmholtzPropagatorComputationalParams(
                                                       terrain_method=TerrainMethod.staircase,
                                                       modify_grid=False,
                                                       two_way=False,
-                                                      max_propagation_angle=85,
+                                                      max_propagation_angle=max_propagation_angle,
                                                       z_order=4,
                                                       exp_pade_coefs=coefs,
                                                       exp_pade_a0_coef=a0,
-                                                      #exp_pade_order=(8, 9),
+                                                      #exp_pade_order=(10, 10),
                                                       dx_wl=dx_wl,
                                                       x_output_filter=1,
                                                       dz_wl=0.25,
@@ -36,6 +40,12 @@ cheb_pade_field = cheb_pade_task.calculate()
 
 pade_vis = FieldVisualiser(cheb_pade_field, env=env, trans_func=lambda v: 20 * cm.log10(1e-16 + abs(v)), x_mult=1E-3, label="Pade")
 plt = pade_vis.plot2d(min=-120, max=0)
+plt.xlabel('Range (km)')
+plt.ylabel('Height (m)')
+plt.tight_layout()
+plt.show()
+
+pade_vis.plot_hor(50.1)
 plt.xlabel('Range (km)')
 plt.ylabel('Height (m)')
 plt.tight_layout()
--- examples/chebyshev_pade/knife_edge2_disp_rel.py
+++ examples/chebyshev_pade/knife_edge2_disp_rel.py
@@ -14,13 +14,13 @@ def k_x_angle(dx, dz, num_coefs, den_coefs, k0, kz_arr):
 freq = 3000e6
 wl = 3e8 / freq
 k0 = 2*fm.pi / wl
-dx_wl = 2
-dz_wl = 0.0000001
-max_angle_degrees = 89
+dx_wl = 5
+dz_wl = 0.000000001
+max_angle_degrees = 85
 coefs, a0 = cheb_pade_coefs(dx_wl, (9, 10), fm.sin(max_angle_degrees*fm.pi/180)**2, 'ratinterp')
 coefs_num_ratinterp = np.array([a[0] for a in coefs])
 coefs_den_ratinterp = np.array([b[1] for b in coefs])
-coefs, a0 = cheb_pade_coefs(dx_wl, (12, 12), fm.sin(max_angle_degrees*fm.pi/180)**2, 'aaa')
+coefs, a0 = cheb_pade_coefs(dx_wl, (10, 10), fm.sin(max_angle_degrees*fm.pi/180)**2, 'aaa')
 coefs_num_aaa = np.array([a[0] for a in coefs])
 coefs_den_aaa = np.array([b[1] for b in coefs])
 kz_arr = np.linspace(0, 1*k0, 10000)
@@ -52,8 +52,8 @@ k_x_aaa = k_x_angle(dx_wl * wl, dz_wl * wl, coefs_num_aaa, coefs_den_aaa, k0, kz
 
 plt.figure(figsize=(6, 3.2))
 # plt.plot(angles, (np.real(k_x_1)), label='opt real')
-plt.plot(kz_arr, (np.real(k_x_r)), label='Pade real')
-plt.plot(kz_arr, (np.real(k_x_ratinterp)), label='opt imag')
+#plt.plot(kz_arr, (np.imag(k_x_r)), label='Pade real')
+plt.plot(kz_arr, (np.imag(k_x_ratinterp)), label='opt imag')
 plt.xlabel('k_z')
 plt.ylabel('k_x abs. error')
 plt.xlim([kz_arr[0], kz_arr[-1]])
