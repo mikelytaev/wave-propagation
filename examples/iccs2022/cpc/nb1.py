9f219a456ae3e8518b0c58acf9c302006cb14946
Mikhail Lytaev <mikelytaev@gmail.com>
2022-08-19 01:14:38 +0300

    pymoo experiments

--- examples/iccs2022/cpc/nb1.py
+++ examples/iccs2022/cpc/nb1.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 import pyximport
+from pymoo.algorithms.soo.nonconvex.g3pcx import G3PCX
+from pymoo.algorithms.soo.nonconvex.nelder import NelderMead
+from pymoo.algorithms.soo.nonconvex.pso import PSO
+
 pyximport.install(setup_args={"include_dirs": np.get_include()}, language_level=3)
 
 from pymoo.core.problem import Problem
@@ -8,6 +12,8 @@ import examples.optimization.evol.opt_utils as opt_utils
 from pymoo.algorithms.soo.nonconvex.de import DE
 from pymoo.operators.sampling.lhs import LHS
 from pymoo.optimize import minimize
+from pymoo.termination.default import DefaultSingleObjectiveTermination
+from pymoo.termination import get_termination
 
 
 class UnconditionalOptimization(Problem):
@@ -33,18 +39,23 @@ problem = UnconditionalOptimization(
     dz_wl=0.25
 )
 
-algorithm = DE(
-    pop_size=100,
+algorithm_de = DE(
+    pop_size=20,
     sampling=LHS(),
-    variant="DE/rand/1/bin",
-    CR=0.3,
+    variant="DE/best/1/exp",
+    CR=0.9,
     dither="vector",
     jitter=True,
 )
 
+algorithm = PSO(pop_size=50, max_velocity_rate=0.001, w=0.9)
+
+termination = get_termination("n_gen", 100000000)
+
 res = minimize(problem,
                algorithm,
                seed=1,
-               verbose=False)
+               termination=termination,
+               verbose=True)
 
 print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
