72218783a73e0e7b9582971f5cb895e00de00dbd
Mikhail Lytaev <mikelytaev@gmail.com>
2022-08-30 19:45:12 +0300

    pymoo experiments

--- examples/iccs2022/jcs/nb2.py
+++ examples/iccs2022/jcs/nb2.py
@@ -5,6 +5,7 @@ from pymoo.algorithms.soo.nonconvex.optuna import Optuna
 from pymoo.core.parameters import set_params, hierarchical
 from pymoo.optimize import minimize
 from pymoo.problems.single import Sphere
+from pymoo.termination import get_termination
 
 from problems import UnconditionalOptimization
 
@@ -16,15 +17,15 @@ problem = UnconditionalOptimization(
     dz_wl=0.25
 )
 
-algorithm = DE()
+algorithm = G3PCX()
 
-n_evals = 500
+termination = get_termination("n_gen", 300)
 
-performance = SingleObjectiveSingleRun(problem, termination=("n_evals", n_evals), seed=1)
+performance = SingleObjectiveSingleRun(problem, termination=termination, seed=1)
 
 res = minimize(HyperparameterProblem(algorithm, performance),
                Optuna(),
-               termination=('n_evals', 50),
+               termination=('n_evals', 100),
                seed=1,
                verbose=True)
 
@@ -32,5 +33,7 @@ hyperparams = res.X
 print(hyperparams)
 set_params(algorithm, hierarchical(hyperparams))
 
-res = minimize(problem, algorithm, termination=("n_evals", n_evals), seed=1, verbose=True)
+termination = get_termination("n_gen", 100000000)
+
+res = minimize(problem, algorithm, termination=termination, seed=1, verbose=True)
 print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
