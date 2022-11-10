bb20726bf51389423ba22398091cd8f502a16b14
Mikhail Lytaev <mikelytaev@gmail.com>
2022-11-10 19:08:06 +0300

    uwa opt experiments

--- examples/optimization/uwa/nb1.py
+++ examples/optimization/uwa/nb1.py
@@ -19,9 +19,9 @@ import propagators._utils as utils
 dx = 5
 order = (7, 8)
 #theta_max_degrees = 20
-xi_bound = 0.8
+xi_bound = 1.1
 xi_bounds = (-xi_bound, 0)
-bounds_ga = [(-2.5, 2.5)] * (order[0] + order[1]) * 2
+bounds_ga = [(-10, 10)] * (order[0] + order[1]) * 2
 
 result_ga = differential_evolution(
     func=opt_utils.fit_func_exp_rational_approx_ga,
@@ -77,30 +77,32 @@ def approx_exp(num_coefs, den_coefs, xi_grid):
     return res
 
 
-grid = np.linspace(-xi_bound, 0.01, 500)
-i_grid, j_grid = np.meshgrid(grid, grid)
+grid_re = np.linspace(-xi_bound*2, 0.1, 500)
+grid_im = np.linspace(-0.1, xi_bound*2, 500)
+i_grid, j_grid = np.meshgrid(grid_re, grid_im)
 xi_grid_2d = i_grid + 1j*j_grid
 shape = xi_grid_2d.shape
 approx_exp_vals = approx_exp(num_coefs, den_coefs, xi_grid_2d.flatten()).reshape(shape)
-# plt.imshow(abs(approx_exp_vals) > 1, extent=[grid[0], grid[-1], grid[0], grid[-1]], cmap=plt.get_cmap('binary'))
-# plt.colorbar()
-# plt.grid(True)
-# plt.show()
+plt.imshow(abs(approx_exp_vals) > 1, extent=[grid_re[0], grid_re[-1], grid_im[0], grid_im[-1]], cmap=plt.get_cmap('binary'))
+plt.title("DE > 1")
+plt.colorbar()
+plt.grid(True)
+plt.show()
 
 errors_de = approx_error(num_coefs, den_coefs, xi_grid_2d.flatten()).reshape(shape)
-# plt.imshow(
-#     10*np.log10(abs(errors_de)),
-#     extent=[grid[0], grid[-1], grid[0], grid[-1]],
-#     cmap=plt.get_cmap('binary'),
-#     norm=Normalize(-90, -40)
-# )
-# plt.colorbar()
-# plt.grid(True)
-# plt.show()
-#
 plt.imshow(
-    np.log10(abs(errors_de)) < -5,
-    extent=[grid[0], grid[-1], grid[0], grid[-1]],
+    10*np.log10(abs(errors_de)),
+    extent=[grid_re[0], grid_re[-1], grid_im[0], grid_im[-1]],
+    cmap=plt.get_cmap('binary'),
+    norm=Normalize(-90, -40)
+)
+plt.colorbar()
+plt.grid(True)
+plt.show()
+
+plt.imshow(
+    np.log10(abs(errors_de)) < -4,
+    extent=[grid_re[0], grid_re[-1], grid_im[0], grid_im[-1]],
     cmap=plt.get_cmap('binary'),
     #norm=Normalize(-9, -5)
 )
@@ -113,22 +115,29 @@ pade_coefs_num = np.array([a[0] for a in pade_coefs])
 pade_coefs_den = np.array([a[1] for a in pade_coefs])
 errors_pade = approx_error(pade_coefs_num, pade_coefs_den, xi_grid_2d.flatten()).reshape(shape)
 plt.imshow(
-    np.log10(abs(errors_pade)) < -5,
-    extent=[grid[0], grid[-1], grid[0], grid[-1]],
+    np.log10(abs(errors_pade)) < -4,
+    extent=[grid_re[0], grid_re[-1], grid_im[0], grid_im[-1]],
     cmap=plt.get_cmap('binary')
 )
 plt.colorbar()
 plt.grid(True)
 plt.show()
 
-xi_grid = np.linspace(xi_bounds[0]*2, -xi_bounds[0]*2, 100)
+pade_exp = approx_exp(pade_coefs_num, pade_coefs_den, xi_grid_2d.flatten()).reshape(shape)
+plt.imshow(abs(pade_exp) > 1, extent=[grid_re[0], grid_re[-1], grid_im[0], grid_im[-1]], cmap=plt.get_cmap('binary'))
+plt.title("Pade > 1")
+plt.colorbar()
+plt.grid(True)
+plt.show()
+
+xi_grid = np.linspace(-xi_bound*2, 0, 100)
 error_de = approx_error(num_coefs, den_coefs, xi_grid)
 error_pade = approx_error(pade_coefs_num, pade_coefs_den, xi_grid)
 
-# plt.figure(figsize=(6, 3.2))
-# plt.plot(xi_grid, 10*np.log10(error_de), label="de")
-# plt.plot(xi_grid, 10*np.log10(error_pade), label="pade")
-# plt.grid(True)
-# plt.tight_layout()
-# plt.legend()
-# plt.show()
+plt.figure(figsize=(6, 3.2))
+plt.plot(xi_grid, 10*np.log10(error_de), label="de")
+plt.plot(xi_grid, 10*np.log10(error_pade), label="pade")
+plt.grid(True)
+plt.tight_layout()
+plt.legend()
+plt.show()
--- examples/optimization/uwa/shallow_water.py
+++ examples/optimization/uwa/shallow_water.py
@@ -9,15 +9,15 @@ from uwa.vis import AcousticPressureFieldVisualiser2d
 
 logging.basicConfig(level=logging.DEBUG)
 
-src = GaussSource(freq_hz=400, depth=20, beam_width=45, eval_angle=0)
+src = GaussSource(freq_hz=500, depth=30, beam_width=45, eval_angle=0)
 env = UnderwaterEnvironment()
-env.sound_speed_profile_m_s = lambda x, z: z*0 + 1500
+env.sound_speed_profile_m_s = lambda x, z: 1500 - z/2
 env.bottom_profile = Bathymetry(ranges_m=[0, 50000], depths_m=[100, 100])
 env.bottom_sound_speed_m_s = 1700
 env.bottom_density_g_cm = 1.5
 env.bottom_attenuation_dm_lambda = 0.5
 
-max_range = 100000
+max_range = 150000
 
 wavelength = 1500 / src.freq_hz
 dr_wl = 10
@@ -27,13 +27,12 @@ sspe_comp_params = HelmholtzPropagatorComputationalParams(
     z_order=4,
     dx_wl=dr_wl,
     dz_wl=dz_wl,
-    exp_pade_order=(4, 4),
-    sqrt_alpha=0
+    exp_pade_order=(8, 8),
+    sqrt_alpha=45
 )
 
-sspe_propagator = UnderwaterAcousticsSSPadePropagator(src=src, env=env, max_range_m=max_range, max_depth_m=200, comp_params=sspe_comp_params)
+sspe_propagator = UnderwaterAcousticsSSPadePropagator(src=src, env=env, max_range_m=max_range, max_depth_m=150, comp_params=sspe_comp_params)
 sspe_field = sspe_propagator.calculate()
 sspe_field.field *= 5.50 #normalization
 sspe_vis = AcousticPressureFieldVisualiser2d(field=sspe_field, label='WPF')
 sspe_vis.plot2d(-50, -5).show()
-
