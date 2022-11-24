dce1867012cea6f446374d2fe016051b21adcb47
Mikhail Lytaev <mikelytaev@gmail.com>
2022-11-24 19:21:18 +0300

    uwa opt experiments

--- examples/optimization/uwa/pade_opt/nb1.py
+++ examples/optimization/uwa/pade_opt/nb1.py
@@ -5,99 +5,85 @@ from propagators._utils import *
 
 
 import matplotlib.pyplot as plt
-dz = 20
-errors_vs_kz = [fourth_order_error_kz(kz, dz) for kz in np.linspace(-fm.pi*3, fm.pi*3, 2000)]
-plt.plot(errors_vs_kz)
-plt.grid(True)
-plt.show()
+# dz = 20
+# errors_vs_kz = [fourth_order_error_kz(kz, dz) for kz in np.linspace(-fm.pi*3, fm.pi*3, 2000)]
+# plt.plot(errors_vs_kz)
+# plt.grid(True)
+#plt.show()
 
-kz = 0.25#*fm.pi
-z_grid = np.linspace(0, 40, 2000)
-errors_vs_dz = [fourth_order_error_kz(kz, dz) for dz in z_grid]
-plt.plot(z_grid, errors_vs_dz)
-plt.grid(True)
-plt.show()
+# kz = 0.25#*fm.pi
+# z_grid = np.linspace(0, 40, 2000)
+# errors_vs_dz = [fourth_order_error_kz(kz, dz) for dz in z_grid]
+# plt.plot(z_grid, errors_vs_dz)
+# plt.grid(True)
+#plt.show()
 
 
 def h_error(dz_wl, k_z_max):
     return fourth_order_error_kz(k_z_max, dz_wl)
 
-def tau_error(xi, dx_wl, dz_wl, pade_coefs):
+
+def tau_error(xi1, xi2, dx_wl, pade_coefs):
     k0 = 2 * fm.pi
-    return abs(cm.exp(1j*k0*dx_wl*(cm.sqrt(1 + xi) - 1)) - np.prod([(1 + a*xi) / (1 + b*xi) for a, b in pade_coefs]))
+    return abs(cm.exp(1j*k0*dx_wl*(cm.sqrt(1 + xi1) - 1)) - fm.prod([(1 + a*xi2) / (1 + b*xi2) for a, b in pade_coefs]))
 
-def precision_step(xi_bounds, dxs_wl: np.array, dzs_wl: np.array, pade_order, z_order=4):
+
+def tau_error_sup_h(xi, xi_bounds, h, n, dx_wl, pade_coefs):
+    xi_grid = np.linspace(xi-h, xi+h, n)
+    xi_grid = xi_grid[xi_grid >= xi_bounds[0]]
+    xi_grid = xi_grid[xi_grid <= xi_bounds[1]]
+    errors = [tau_error(xi, xi2, dx_wl, pade_coefs) for xi2 in xi_grid]
+    return max(errors)
+
+
+def precision_step(xi_bounds, k_z_max, dxs_wl: np.array, dzs_wl: np.array, pade_order):
     k0 = 2 * fm.pi
-    for dx_wl in dxs_wl:
-        coefs = pade_propagator_coefs(diff2=lambda x: x, k0=k0, dx=dx_wl)
-        for dz_wl in dzs_wl:
-            pass
-
-
-
-    k0 = 2*cm.pi
-    res = (None, None, None)
-    cur_min = 1e100
-
-    if pade_order:
-        pade_orders = [pade_order]
-    else:
-        pade_orders = [(7, 8), (6, 7), (5, 6), (4, 5), (3, 4), (2, 3), (1, 2), (1, 1)]
-
-    if dx_wl:
-        dxs = [dx_wl]
-    else:
-        dxs = np.concatenate((#np.linspace(0.001, 0.01, 10),
-                              #np.linspace(0.02, 0.1, 9),
-                              #np.linspace(0.2, 1, 9),
-                              #np.linspace(2, 10, 9),
-                              np.linspace(20, 100, 9),
-                              np.linspace(200, 1000, 9),
-                              np.linspace(1100, 1900, 9),
-                              np.linspace(2000, 10000, 9)))
-
-    if dz_wl:
-        dzs = [dz_wl]
-    else:
-        dzs = np.concatenate((np.array([0.001, 0.009]),
-                              np.array([0.01, 0.09]),
-                             np.linspace(0.1, 9, 90)))
-
-    dxs.sort()
-    dzs.sort()
-    for pade_order in pade_orders:
-        for dx_wl in dxs:
-            updated = False
-            if z_order <= 4:
-                coefs = pade_propagator_coefs(pade_order=pade_order, diff2=lambda x: x, k0=k0, dx=dx_wl, spe=False)
-            for dz_wl in dzs:
-                if z_order > 4:
-                    coefs = pade_propagator_coefs(pade_order=pade_order,
-                                                  diff2=lambda s: mpmath.acosh(1 + (k0 * dz_wl) ** 2 * s / 2) ** 2 / (k0 * dz_wl) ** 2,
-                                                  k0=k0, dx=dx_wl, spe=False)
-
-                errors = []
-                for al in np.linspace(0, max_angle_deg, 20):
-                    kz = k0 * cm.sin(al * cm.pi / 180)
-                    if z_order <= 4:
-                        discrete_kx = discrete_k_x(k0, dx_wl, coefs, dz_wl, kz, order=z_order)
-                    else:
-                        discrete_kx = discrete_k_x(k0, dx_wl, coefs, dz_wl, kz, order=2)
-                    real_kx = cm.sqrt(k0 ** 2 - kz ** 2)
-                    errors += [abs(real_kx - discrete_kx) / k0]
-
-                val = pade_order[1] / (dx_wl * dz_wl)
-                error = max(errors)
-
-                if error >= threshold * dx_wl / max_distance_wl:
-                    break
-
-                if error < threshold * dx_wl / max_distance_wl and val < cur_min:
-                    res = (dx_wl, dz_wl, pade_order)
-                    cur_min = val
-                    updated = True
-
-            if not updated:
-                break
-
+    res = np.zeros((len(dxs_wl), len(dzs_wl)))
+    xi_grid = np.linspace(xi_bounds[0], xi_bounds[1], 100)
+    for dx_i, dx_wl in enumerate(dxs_wl):
+        coefs = pade_propagator_coefs(pade_order=pade_order, diff2=lambda x: x, k0=k0, dx=dx_wl)
+        for dz_i, dz_wl in enumerate(dzs_wl):
+            h = h_error(dz_wl, k_z_max) / k0**2
+            error = max([tau_error_sup_h(xi, xi_bounds, h, 100, dx_wl, coefs) for xi in xi_grid])
+            res[dx_i, dz_i] = error
     return res
+
+
+def get_optimal(x_max_wl, prec, xi_min, k_z_max):
+    pade_order = (7, 8)
+    dxs_wl = np.concatenate((
+        np.linspace(0.1, 1, 10),
+        np.linspace(2, 10, 9),
+        np.linspace(20, 100, 9),
+    ))
+    dzs_wl = np.concatenate((
+        np.linspace(0.001, 0.01, 10),
+        np.linspace(0.02, 0.1, 9),
+        np.linspace(0.2, 1, 9),
+    ))
+    errors = precision_step([xi_min, 0], k_z_max, dxs_wl, dzs_wl, pade_order)
+    cur_best_dx = 1e-16
+    cur_best_dz = 1e-16
+    for dx_i, dx in enumerate(dxs_wl):
+        print(dx_i)
+        for dz_i, dz in enumerate(dzs_wl):
+            err = errors[dx_i, dz_i]
+            if x_max_wl / dx * err < prec:
+                if dx * dz > cur_best_dx * cur_best_dz:
+                    cur_best_dx, cur_best_dz = dx, dz
+
+    return cur_best_dx, cur_best_dz
+
+
+k0 = 2*fm.pi
+theta_max_degrees = 30
+k_z_max = k0*fm.sin(theta_max_degrees*fm.pi/180)
+xi_bounds = [-k_z_max**2/k0**2, 0]
+# z_grid = np.linspace(0.01, 5, 100)
+# errors = precision_step(xi_bounds, k_z_max, [10], z_grid, (7, 8))
+#
+# plt.plot(z_grid, 10*np.log10(errors[0]))
+# plt.grid(True)
+# plt.show()
+
+dx, dz = get_optimal(10000, 1e-2, xi_bounds[0], k_z_max)
