006c54a654fa308debcb7ce3a4ce9539e134e06f
Mikhail Lytaev <mikelytaev@gmail.com>
2022-11-25 19:50:59 +0300

    uwa opt experiments

--- examples/optimization/uwa/pade_opt/nb1.py
+++ examples/optimization/uwa/pade_opt/nb1.py
@@ -1,4 +1,10 @@
 import numpy as np
+
+import pyximport
+pyximport.install(setup_args={"include_dirs": np.get_include()}, language_level=3)
+
+import utils as utils
+
 from examples.jcs.d2_error import *
 import math as fm
 from propagators._utils import *
@@ -28,11 +34,11 @@ def tau_error(xi1, xi2, dx_wl, pade_coefs):
     return abs(cm.exp(1j*k0*dx_wl*(cm.sqrt(1 + xi1) - 1)) - fm.prod([(1 + a*xi2) / (1 + b*xi2) for a, b in pade_coefs]))
 
 
-def tau_error_sup_h(xi, xi_bounds, h, n, dx_wl, pade_coefs):
+def tau_error_sup_h(xi, xi_bounds, h, n, dx_wl, pade_coefs_num, pade_coefs_den):
     xi_grid = np.linspace(xi-h, xi+h, n)
     xi_grid = xi_grid[xi_grid >= xi_bounds[0]]
     xi_grid = xi_grid[xi_grid <= xi_bounds[1]]
-    errors = [tau_error(xi, xi2, dx_wl, pade_coefs) for xi2 in xi_grid]
+    errors = [utils.tau_error(xi, xi2, dx_wl, pade_coefs_num, pade_coefs_den) for xi2 in xi_grid]
     return max(errors)
 
 
@@ -42,21 +48,28 @@ def precision_step(xi_bounds, k_z_max, dxs_wl: np.array, dzs_wl: np.array, pade_
     xi_grid = np.linspace(xi_bounds[0], xi_bounds[1], 100)
     for dx_i, dx_wl in enumerate(dxs_wl):
         coefs = pade_propagator_coefs(pade_order=pade_order, diff2=lambda x: x, k0=k0, dx=dx_wl)
+        pade_coefs_num = np.array([a[0] for a in coefs])
+        pade_coefs_den = np.array([a[1] for a in coefs])
         for dz_i, dz_wl in enumerate(dzs_wl):
             h = h_error(dz_wl, k_z_max) / k0**2
-            error = max([tau_error_sup_h(xi, xi_bounds, h, 100, dx_wl, coefs) for xi in xi_grid])
+            error = max([tau_error_sup_h(xi, xi_bounds, h, 100, dx_wl, pade_coefs_num, pade_coefs_den) for xi in xi_grid])
             res[dx_i, dz_i] = error
     return res
 
 
 def get_optimal(x_max_wl, prec, xi_min, k_z_max):
-    pade_order = (7, 8)
+    pade_order = (8, 8)
     dxs_wl = np.concatenate((
+        #[0.0005],
+        #[0.001],
+        #[0.01],
         np.linspace(0.1, 1, 10),
         np.linspace(2, 10, 9),
         np.linspace(20, 100, 9),
     ))
     dzs_wl = np.concatenate((
+        #[0.00001],
+        #[0.0001],
         np.linspace(0.001, 0.01, 10),
         np.linspace(0.02, 0.1, 9),
         np.linspace(0.2, 1, 9),
@@ -65,7 +78,6 @@ def get_optimal(x_max_wl, prec, xi_min, k_z_max):
     cur_best_dx = 1e-16
     cur_best_dz = 1e-16
     for dx_i, dx in enumerate(dxs_wl):
-        print(dx_i)
         for dz_i, dz in enumerate(dzs_wl):
             err = errors[dx_i, dz_i]
             if x_max_wl / dx * err < prec:
@@ -76,7 +88,7 @@ def get_optimal(x_max_wl, prec, xi_min, k_z_max):
 
 
 k0 = 2*fm.pi
-theta_max_degrees = 30
+theta_max_degrees = 70
 k_z_max = k0*fm.sin(theta_max_degrees*fm.pi/180)
 xi_bounds = [-k_z_max**2/k0**2, 0]
 # z_grid = np.linspace(0.01, 5, 100)
@@ -87,3 +99,4 @@ xi_bounds = [-k_z_max**2/k0**2, 0]
 # plt.show()
 
 dx, dz = get_optimal(10000, 1e-2, xi_bounds[0], k_z_max)
+print(f"dx = {dx}; dz = {dz}")
---
+++ examples/optimization/uwa/pade_opt/utils.pyx
@@ -0,0 +1,21 @@
+import numpy as np
+cimport numpy as np
+
+from math import pi
+from cmath import sqrt, exp
+
+
+def tau_error(complex xi1, complex xi2, double dx_wl, np.ndarray[complex, ndim=1] pade_coefs_num, np.ndarray[complex, ndim=1] pade_coefs_den):
+    cdef complex product = 1
+    cdef complex a_i = 0
+    cdef Py_ssize_t i
+    for i in range(0, len(pade_coefs_den)):
+        if i < len(pade_coefs_num):
+            a_i = pade_coefs_num[i]
+        else:
+            a_i = 0
+        product *= (1 + a_i * xi2) / (1 + pade_coefs_den[i] * xi2)
+
+    cdef k0 = 2 * pi
+    cdef complex ex = exp(1j * k0 * dx_wl * (sqrt(1.0 + xi1) - 1.0))
+    return abs(ex - product).real
