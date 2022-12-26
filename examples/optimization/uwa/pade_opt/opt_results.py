7b7510302af2ee5ed52002e2ea823c47c4a4f432
Mikhail Lytaev <mikelytaev@gmail.com>
2022-12-26 19:05:04 +0300

    uwa opt experiments

--- examples/optimization/uwa/pade_opt/opt_results.py
+++ examples/optimization/uwa/pade_opt/opt_results.py
@@ -31,7 +31,7 @@ def func(max_range_m, theta_max_degrees):
     )
     print(f"c0 = {c0}; xi = {xi_bounds}; Pade: dx = {dr_wl} m; dz = {dz_wl} m")
 
-    dr_wl, dz_wl, c0, xi_bounds = get_optimal(
+    dr_wl_s, dz_wl_s, c0, xi_bounds = get_optimal(
         freq_hz=freq_hz,
         x_max_m=max_range_m,
         prec=prec,
@@ -40,7 +40,9 @@ def func(max_range_m, theta_max_degrees):
         c_bounds=[c_min, c_max],
         return_meta=True
     )
-    print(f"c0 = {c0}; xi = {xi_bounds}; Pade (shifted): dx = {dr_wl} m; dz = {dz_wl} m")
+    print(f"c0 = {c0}; xi = {xi_bounds}; Pade (shifted): dx = {dr_wl_s} m; dz = {dz_wl_s} m")
+    gain = (dr_wl_s * dz_wl_s) / (dr_wl * dz_wl)
+    print(f"gain = {gain}")
 
 
 func(max_range_m=1000, theta_max_degrees=30)
