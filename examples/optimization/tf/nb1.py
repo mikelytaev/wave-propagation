1c892c1ec412fb1370633b07e7ae1b65d0b84472
Mikhail Lytaev <mikelytaev@gmail.com>
2023-01-28 03:17:07 +0300

    tf experiments

--- examples/optimization/tf/nb1.py
+++ examples/optimization/tf/nb1.py
@@ -5,30 +5,41 @@ import tensorflow_probability as tfp
 import math as fm
 
 
-x = tf.Variable([0.1j, 0.1j], tf.complex128)
-y = tf.Variable([0.1j, 0.1j], tf.complex128)
+
 
 rational_order = (7, 7)
-num_coef = tf.reshape(tf.Variable([0.1j] * rational_order[0], tf.complex128), (rational_order[0], 1))
-den_coef = tf.reshape(tf.Variable([0.1j] * rational_order[1], tf.complex128), (rational_order[1], 1))
+num_coef = tf.Variable(tf.reshape(tf.Variable(np.random.rand(rational_order[0])+np.random.rand(rational_order[0])*1j, tf.complex128), (rational_order[0], 1)))
+den_coef = tf.Variable(tf.reshape(tf.Variable(np.random.rand(rational_order[1])+np.random.rand(rational_order[1])*1j, tf.complex128), (rational_order[1], 1)))
 k0 = 2 * fm.pi
-dx = 10
-dz = 0.5
-theta_max_degrees = 10
-k_z_max = k0 * fm.sin(theta_max_degrees)
-k_z_grid = tf.reshape(tf.constant(np.linspace(0, k_z_max, 100)), (1, 100))
-loss_fn = lambda: (tf.math.log(tf.norm(x)) + 2*tf.norm(y) - 5.)**2
+dx = 50
+dz = 0.25
+theta_max_degrees = 22
+k_z_max = k0 * fm.sin(theta_max_degrees*fm.pi/180)
+k_z_grid = np.linspace(0, k_z_max, 100)+0j
+k_z_grid_tf = tf.reshape(tf.constant(k_z_grid, tf.complex128), (1, len(k_z_grid)))
+d_k_z = k_z_grid[1] - k_z_grid[0]
 
-loss_fn2 = lambda: loss_fn() + 2*loss_fn()
 
-t_i = lambda k_z, a_i, b_i: (1 - 4*a_i/(k0*dz)**2*tf.math.sin(k_z*dz/2)**2) / (1 - 4*b_i/(k0*dz)**2*tf.math.sin(k_z*dz/2)**2)
-de_numerator = lambda a: tf.reduce_sum(tf.math.log((1 - 4*a/(k0*dz)**2*tf.math.sin(k_z_grid*dz/2)**2)), axis=0)
+de_numerator = lambda a: tf.reduce_sum(tf.math.log((1 - 4 * a / (k0*dz) ** 2 * tf.math.sin(k_z_grid_tf * dz / 2) ** 2)), axis=0)
 
-discrete_k_x = lambda k_z: k0 + (de_numerator(k_z, num_coef) - de_numerator(k_z, den_coef)) / (1j * dx)
-#loss_fn3 = lambda k_z:
+discrete_k_x = lambda: k0 + (de_numerator(num_coef) - de_numerator(den_coef)) / (1j * dx)
+loss_fn3 = lambda: tf.abs(tf.reduce_sum((discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)) ** 2, axis=1) * d_k_z)
+loss_fn4 = lambda: tf.reduce_max(tf.abs(discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)))
 
-losses = tfp.math.minimize(loss_fn2,
-                           num_steps=200,
-                           optimizer=tf.optimizers.Adam(learning_rate=0.1))
+trace_fn = lambda traceable_quantities: {
+  'loss': traceable_quantities.loss, 'num_coef': num_coef}
+losses = tfp.math.minimize(loss_fn4,
+                           num_steps=20000,
+                           optimizer=tf.optimizers.Adam(learning_rate=10, epsilon=0.1),
+                           trainable_variables=[num_coef, den_coef],
+                           #trace_fn=trace_fn
+                           )
 
-print("optimized value is {} with loss {}".format(x, losses[-1]))
+print("optimized value is {} with loss {}".format(num_coef, losses[-1]))
+
+# Setup a stochastic gradient descent optimizer
+# opt = tf.keras.optimizers.SGD(learning_rate=0.01)
+#
+# # Optimize for a fixed number of steps
+# for _ in range(1000):
+#     opt.minimize(loss_fn3, [num_coef, den_coef])
