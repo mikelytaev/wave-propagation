96709273839b08118415c1707d3fa9d255b29b8c
Mikhail Lytaev <mikelytaev@gmail.com>
2023-01-31 03:24:55 +0300

    tf experiments

--- examples/optimization/tf/nb1.py
+++ examples/optimization/tf/nb1.py
@@ -23,13 +23,13 @@ d_k_z = k_z_grid[1] - k_z_grid[0]
 de_numerator = lambda a: tf.reduce_sum(tf.math.log((1 - 4 * a / (k0*dz) ** 2 * tf.math.sin(k_z_grid_tf * dz / 2) ** 2)), axis=0)
 
 discrete_k_x = lambda: k0 + (de_numerator(num_coef) - de_numerator(den_coef)) / (1j * dx)
-loss_fn3 = lambda: tf.abs(tf.reduce_sum((discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)) ** 2, axis=1) * d_k_z)
+loss_fn3 = lambda: tf.abs(tf.sqrt(tf.reduce_sum((discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)) ** 2, axis=1)) * d_k_z)
 loss_fn4 = lambda: tf.reduce_max(tf.abs(discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)))
 
 trace_fn = lambda traceable_quantities: {
   'loss': traceable_quantities.loss, 'num_coef': num_coef}
-losses = tfp.math.minimize(loss_fn3,
-                           num_steps=20000,
+losses = tfp.math.minimize(loss_fn4,
+                           num_steps=100000,
                            optimizer=tf.optimizers.Adam(learning_rate=10, epsilon=0.1),
                            trainable_variables=[num_coef, den_coef],
                            #trace_fn=trace_fn
