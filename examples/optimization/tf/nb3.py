6323a595a802def5bd29325777afc97ea07658bb
Mikhail Lytaev <mikelytaev@gmail.com>
2023-02-28 19:17:46 +0300

    tf experiments

--- examples/optimization/tf/nb3.py
+++ examples/optimization/tf/nb3.py
@@ -15,7 +15,7 @@ dx = 50
 dz = 0.25
 theta_max_degrees = 22
 k_z_max = k0 * fm.sin(theta_max_degrees*fm.pi/180)
-k_z_grid = np.linspace(0, k_z_max, 100)+0j
+k_z_grid = np.linspace(0, k_z_max, 20)+0j
 k_z_grid_tf = tf.reshape(tf.constant(k_z_grid, tf.complex128), (1, len(k_z_grid)))
 d_k_z = k_z_grid[1] - k_z_grid[0]
 
@@ -26,26 +26,13 @@ discrete_k_x = lambda: k0 + (de_numerator(num_coef) - de_numerator(den_coef)) / 
 loss_fn3 = lambda: tf.abs(tf.sqrt(tf.reduce_sum((discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)) ** 2, axis=1)) * d_k_z)
 loss_fn4 = lambda: tf.reduce_max(tf.abs(discrete_k_x() - tf.math.sqrt(k0 ** 2 - k_z_grid_tf ** 2)))
 
-# trace_fn = lambda traceable_quantities: {
-#   'loss': traceable_quantities.loss, 'num_coef': num_coef}
-# losses = tfp.math.minimize(loss_fn4,
-#                            num_steps=300000,
-#                            optimizer=tf.optimizers.Adam(learning_rate=0.5, epsilon=0.1),
-#                            trainable_variables=[num_coef, den_coef],
-#                            #trace_fn=trace_fn
-#                            )
-#
-# print("optimized value is {} with loss {}".format(num_coef, losses[-1]))
+trace_fn = lambda traceable_quantities: {
+  'loss': traceable_quantities.loss, 'num_coef': num_coef}
+losses = tfp.math.minimize(loss_fn4,
+                           num_steps=100000,
+                           optimizer=tf.optimizers.Adam(learning_rate=0.1, epsilon=0.1),
+                           trainable_variables=[num_coef, den_coef],
+                           #trace_fn=trace_fn
+                           )
 
-optimizer = tf.optimizers.SGD(learning_rate=0.5, nesterov=True)
-#optimizer = tf.optimizers.Adam(learning_rate=0.5, epsilon=0.1)
-
-for i in range(20000):
-  with tf.GradientTape() as tape:
-    loss = loss_fn4()
-  print(loss)
-  grads = tape.gradient(loss, [num_coef, den_coef])
-  #print(num_coef)
-  optimizer.apply_gradients(zip(grads, [num_coef, den_coef]))
-
-print("Minimum value: ", loss_fn4())
+print("optimized value is {} with loss {}".format(num_coef, losses[-1]))
