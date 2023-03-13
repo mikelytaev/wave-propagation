39ec7fe0cd1517ead3320cffb955453d4c7f0383
Mikhail Lytaev <mikelytaev@gmail.com>
2023-03-14 02:07:45 +0300

    3d dispersion relation experiments

--- examples/3d/dispersion.py
+++ examples/3d/dispersion.py
@@ -8,12 +8,12 @@ from matplotlib.colors import Normalize
 
 n = 200
 k0 = 2 * fm.pi
-dx = 60
-dy = 1
-dz = 1
+dx = 4
+dy = 0.01
+dz = 0.01
 order = (6, 7)
 phi_max_degrees = 30
-theta_max_degrees = 20
+theta_max_degrees = 30
 k_y_min = -k0 * fm.sin(fm.radians(theta_max_degrees))
 k_y_max = k0 * fm.sin(fm.radians(theta_max_degrees))
 k_z_min = -k0 * fm.sin(fm.radians(theta_max_degrees))
@@ -35,12 +35,12 @@ def xi2(alpha=0.0):
 
 
 def discrete_k_x_2d(alpha=0.0):
-    xi = xi2(alpha)
-    pade_coefs, c0 = utils.pade_propagator_coefs(pade_order=order, diff2=lambda x: x, k0=k0, dx=dx, a0=0)
+    xi = xi2(0)
+    pade_coefs, c0 = utils.pade_propagator_coefs(pade_order=order, diff2=lambda x: x, k0=k0, dx=dx, a0=-alpha)
     t = c0
     for (a, b) in pade_coefs:
         t *= (1 + a*xi) / (1 + b*xi)
-    return k0*fm.sqrt(1-alpha) - 1j/dx * np.log(t)
+    return k0*fm.sqrt(1-0) - 1j/dx * np.log(t)
 
 
 def k_x_2d():
@@ -51,7 +51,7 @@ def k_x_2d():
 
 
 err1 = 10*np.log10(np.abs((k_x_2d() - discrete_k_x_2d())/k_x_2d()))
-err2 = 10*np.log10(np.abs((k_x_2d() - discrete_k_x_2d(alpha=-fm.sin(fm.radians(theta_max_degrees))**2/2))/k_x_2d()))
+err2 = 10*np.log10(np.abs((k_x_2d() - discrete_k_x_2d(alpha=0.125))/k_x_2d()))
 
 # extent = [k_y_min/k0, k_y_max/k0, k_z_min/k0, k_z_max/k0]
 # norm = Normalize(0, -30)
@@ -61,11 +61,11 @@ err2 = 10*np.log10(np.abs((k_x_2d() - discrete_k_x_2d(alpha=-fm.sin(fm.radians(t
 
 
 k_y_grid = np.linspace(k_y_min, k_y_max, n)
-# plt.plot(k_y_grid/k0, err1[100,:], k_y_grid/k0, err2[100,:])
-# plt.grid(True)
-# plt.show()
+plt.plot(k_y_grid/k0, err1[100,:], k_y_grid/k0, err2[100,:])
+plt.grid(True)
+plt.show()
 
 
-plt.plot(k_y_grid, k_x_2d()[100,:], k_y_grid, discrete_k_x_2d(alpha=0.1)[100,:])
+plt.plot(k_y_grid/k0, k_x_2d()[100,:], k_y_grid/k0, discrete_k_x_2d(alpha=0.125)[100,:])
 plt.grid(True)
 plt.show()
