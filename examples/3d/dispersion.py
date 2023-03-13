2109503adc1d703b87dbb6930b33908151a8fe50
Mikhail Lytaev <mikelytaev@gmail.com>
2023-03-13 19:49:31 +0300

    3d experiments

--- examples/3d/dispersion.py
+++ examples/3d/dispersion.py
@@ -3,48 +3,69 @@ import cmath as cm
 import numpy as np
 import propagators._utils as utils
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
 
 
 n = 200
 k0 = 2 * fm.pi
-dx = 10
+dx = 60
 dy = 1
 dz = 1
 order = (6, 7)
-alpha = 0
 phi_max_degrees = 30
-theta_max_degrees = 60
-k_y_min = 0
-k_y_max = k0 * fm.cos(fm.radians(phi_max_degrees)) * fm.sin(fm.radians(theta_max_degrees))
-k_z_min = 0
-k_z_max = k0 * fm.sin(fm.radians(phi_max_degrees)) * fm.sin(fm.radians(theta_max_degrees))
+theta_max_degrees = 20
+k_y_min = -k0 * fm.sin(fm.radians(theta_max_degrees))
+k_y_max = k0 * fm.sin(fm.radians(theta_max_degrees))
+k_z_min = -k0 * fm.sin(fm.radians(theta_max_degrees))
+k_z_max = k0 * fm.sin(fm.radians(theta_max_degrees))
 
 
-def xi1():
+def xi1(alpha=0.0):
     k_y_grid = np.linspace(k_y_min, k_y_max, n)
     k_z_grid = np.linspace(k_z_min, k_z_max, n)
     k_y_2d_grid, k_z_2d_grid = np.meshgrid(k_y_grid, k_z_grid, indexing='ij')
     return -(k_y_2d_grid / k0) ** 2 - (k_z_2d_grid / k0) ** 2 + alpha
 
 
-def discrete_k_x():
-    xi = xi1()
-    pade_coefs, c0 = utils.pade_propagator_coefs(pade_order=order, diff2=lambda x: x, k0=k0, dx=dx)
-    t = 1.0 + 0.0j
+def xi2(alpha=0.0):
+    k_y_grid = np.linspace(k_y_min, k_y_max, n)
+    k_z_grid = np.linspace(k_z_min, k_z_max, n)
+    k_y_2d_grid, k_z_2d_grid = np.meshgrid(k_y_grid, k_z_grid, indexing='ij')
+    return -(2*np.sin(k_y_2d_grid*dy/2) / (k0*dy))**2 - (2*np.sin(k_z_2d_grid*dz/2) / (k0*dz))**2 + alpha
+
+
+def discrete_k_x_2d(alpha=0.0):
+    xi = xi2(alpha)
+    pade_coefs, c0 = utils.pade_propagator_coefs(pade_order=order, diff2=lambda x: x, k0=k0, dx=dx, a0=0)
+    t = c0
     for (a, b) in pade_coefs:
         t *= (1 + a*xi) / (1 + b*xi)
     return k0*fm.sqrt(1-alpha) - 1j/dx * np.log(t)
 
 
-def k_x():
-    k_y_grid = np.linspace(0, k_y_max, n)
-    k_z_grid = np.linspace(0, k_z_max, n)
+def k_x_2d():
+    k_y_grid = np.linspace(k_y_min, k_y_max, n)
+    k_z_grid = np.linspace(k_z_min, k_z_max, n)
     k_y_2d_grid, k_z_2d_grid = np.meshgrid(k_y_grid, k_z_grid, indexing='ij')
     return np.sqrt(k0**2 - k_y_2d_grid**2 - k_z_2d_grid**2)
 
 
-err = 10*np.log10(np.abs(discrete_k_x() - k_x()))
-extent = [k_y_min, k_y_max, k_z_min, k_z_max]
-plt.imshow(err)
-plt.colorbar(fraction=0.046, pad=0.04)
+err1 = 10*np.log10(np.abs((k_x_2d() - discrete_k_x_2d())/k_x_2d()))
+err2 = 10*np.log10(np.abs((k_x_2d() - discrete_k_x_2d(alpha=-fm.sin(fm.radians(theta_max_degrees))**2/2))/k_x_2d()))
+
+# extent = [k_y_min/k0, k_y_max/k0, k_z_min/k0, k_z_max/k0]
+# norm = Normalize(0, -30)
+# plt.imshow(err1, extent=extent, norm=norm, cmap=plt.get_cmap('binary'))
+# plt.colorbar(fraction=0.046, pad=0.04)
+# plt.show()
+
+
+k_y_grid = np.linspace(k_y_min, k_y_max, n)
+# plt.plot(k_y_grid/k0, err1[100,:], k_y_grid/k0, err2[100,:])
+# plt.grid(True)
+# plt.show()
+
+
+plt.plot(k_y_grid, k_x_2d()[100,:], k_y_grid, discrete_k_x_2d(alpha=0.1)[100,:])
+plt.grid(True)
 plt.show()
---
+++ examples/3d/opt/ex1.py
@@ -0,0 +1,85 @@
+import cmath as cm
+
+import math as fm
+import numpy as np
+
+from examples.jcs.d2_error import second_order_error_kz, fourth_order_error_kz
+from propagators._utils import pade_propagator_coefs
+import propagators._utils as utils
+
+
+def precision_step(k0, theta_max_degrees, dxs_m, dzs_m, pade_order, alpha=0):
+    res = np.zeros((len(dxs_m), len(dzs_m)))
+    n = 100
+    t_grid = np.linspace(0, 2*fm.pi, n)
+    r_grid = np.linspace(0, k0*fm.sin(fm.radians(theta_max_degrees)), n)
+    k_y_grid = np.sin(t_grid) * r_grid
+    k_z_grid = np.cos(t_grid) * r_grid
+    #xi_grid = -(k_y_grid/k0)**2 - (k_z_grid/k0)**2
+    for dx_i, dx_m in enumerate(dxs_m):
+        pade_coefs, c0 = utils.pade_propagator_coefs(pade_order=pade_order, diff2=lambda x: x, k0=k0, dx=dx_m, a0=0)
+        for dz_i, dz_m in enumerate(dzs_m):
+            xi_grid = -(2*np.sin(k_y_grid*dz_m/2) / (k0*dz_m))**2 - (2*np.sin(k_z_grid*dz_m/2) / (k0*dz_m))**2 + alpha
+            t = c0
+            for (a, b) in pade_coefs:
+                t *= (1 + a * xi_grid) / (1 + b * xi_grid)
+            discrete_k_x = k0*fm.sqrt(1-alpha) - 1j/dx_m * np.log(t)
+            k_x = np.sqrt(k0**2 - k_y_grid**2 - k_z_grid**2)
+            res[dx_i, dz_i] = np.max(np.abs(k_x - discrete_k_x))
+    return res
+
+
+def get_optimal(*, freq_hz, x_max_m, prec, theta_max_degrees, pade_order, z_order=4, shift=False, return_meta=False):
+    alpha = fm.sin(fm.radians(theta_max_degrees))**2 / 2 if shift else 0
+    k0 = 2 * fm.pi * freq_hz / 3E8
+    dxs_m = np.concatenate((
+        #[0.0005],
+        #[0.001],
+        [0.01],
+        np.linspace(0.1, 1, 10),
+        np.linspace(2, 10, 9),
+        np.linspace(20, 100, 9),
+    ))
+    dzs_m = np.concatenate((
+        #[0.00001],
+        [0.0001],
+        np.linspace(0.001, 0.01, 10),
+        np.linspace(0.02, 0.1, 9),
+        np.linspace(0.2, 1, 9),
+    ))
+    errors = precision_step(k0, theta_max_degrees, dxs_m, dzs_m, pade_order, alpha=alpha)
+    cur_best_dx = 1e-16
+    cur_best_dz = 1e-16
+    for dx_i, dx in enumerate(dxs_m):
+        for dz_i, dz in enumerate(dzs_m):
+            err = errors[dx_i, dz_i]
+            if x_max_m / dx * err < prec:
+                if dx * dz > cur_best_dx * cur_best_dz:
+                    cur_best_dx, cur_best_dz = dx, dz
+
+    if return_meta:
+        return cur_best_dx, cur_best_dz, xi_bounds
+    else:
+        return cur_best_dx, cur_best_dz
+
+
+dx, dz = get_optimal(
+    freq_hz=300e6,
+    x_max_m=300,
+    prec=5e-2,
+    theta_max_degrees=10,
+    pade_order=(6, 7),
+)
+
+print(f'dx={dx}; dz={dz}')
+
+dx, dz = get_optimal(
+    freq_hz=300e6,
+    x_max_m=300,
+    prec=5e-2,
+    theta_max_degrees=10,
+    pade_order=(6, 7),
+    shift=True
+)
+
+print(f'dx={dx}; dz={dz}')
