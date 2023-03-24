c45800b5014ae9ea4dd90fe29187b5f5a091d560
Mikhail Lytaev <mikelytaev@gmail.com>
2023-03-24 18:59:57 +0300

    3d experiments

--- examples/3d/opt/ex6.py
+++ examples/3d/opt/ex6.py
@@ -10,7 +10,7 @@ theta_max_degrees = 30
 theta_grid_degrees, err = err_theta(
     freq_hz=300e6,
     dx_m=3.69*1.95,
-    dz_m=0.076,
+    dz_m=0.09,
     theta_max_degrees=theta_max_degrees,
     phi_degrees=10,
     pade_order=(6, 7),
@@ -23,7 +23,7 @@ theta_grid_degrees, err = err_theta(
 theta_grid_degrees, err_shift = err_theta(
     freq_hz=300e6,
     dx_m=3.69*1.95,
-    dz_m=0.076,
+    dz_m=0.09,
     theta_max_degrees=theta_max_degrees,
     phi_degrees=10,
     pade_order=(6, 7),
@@ -35,10 +35,12 @@ theta_grid_degrees, err_shift = err_theta(
 
 
 f, ax = plt.subplots(1, 1, figsize=(6, 3.2), constrained_layout=True)
-ax.plot(theta_grid_degrees, 10*np.log10(err), theta_grid_degrees, 10*np.log10(err_shift))
+ax.plot(theta_grid_degrees, 10*np.log10(err), label="β=k")
+ax.plot(theta_grid_degrees, 10*np.log10(err_shift), label="optimized β")
 ax.grid(True)
 ax.set_xlim([0, theta_max_degrees])
 ax.set_ylim([-100, 0])
 ax.set_ylabel("k_x error")
 ax.set_xlabel("theta (degrees)")
+ax.legend(loc='lower right')
 plt.show()
