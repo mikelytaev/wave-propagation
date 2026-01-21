90ded1a5ad5acd00e0d14229352ba9d803450fa6
Konrad <konradha@yahoo.com>
2026-01-21 19:35:10 +0100

    Small fix for MacOS pip env

--- setup.py
+++ setup.py
@@ -1,45 +1,28 @@
-from setuptools import setup
-from setuptools.extension import Extension
-import numpy
+from setuptools import setup, Extension
 
-USE_CYTHON = True
-ext = '.pyx' if USE_CYTHON else '.c'
-
-extensions = [Extension("propagators.contfrac", ["propagators/contfrac"+ext], include_dirs=['.', numpy.get_include()]),
-              Extension("propagators._cn_utils", ["propagators/_cn_utils"+ext], include_dirs=['.', numpy.get_include()]),
-              Extension("propagators.dispersion_relations", ["propagators/dispersion_relations"+ext], include_dirs=['.'])
-              ]
-
-if USE_CYTHON:
+def build_extensions():
     from Cython.Build import cythonize
-    extensions = cythonize(extensions, language_level="3")
+    import numpy
+    
+    np_inc = numpy.get_include()
+    
+    extensions = [
+        Extension(
+            "pywaveprop.propagators.contfrac",
+            ["pywaveprop/propagators/contfrac.pyx"],
+            include_dirs=['.', np_inc],
+        ),
+        Extension(
+            "pywaveprop.propagators._cn_utils",
+            ["pywaveprop/propagators/_cn_utils.pyx"],
+            include_dirs=['.', np_inc],
+        ),
+        Extension(
+            "pywaveprop.propagators.dispersion_relations",
+            ["pywaveprop/propagators/dispersion_relations.pyx"],
+            include_dirs=['.', np_inc],
+        ),
+    ]
+    return cythonize(extensions, language_level="3")
 
-setup(
-    name='pywaveprop',
-    version='1.0.0',
-    url='https://github.com/mikelytaev/wave-propagation',
-    license='MIT',
-    author='Mikhail Lytaev',
-    author_email='mikelytaev@gmail.com',
-    description='Wave propagation framework',
-    setup_requires=[
-        'numpy'
-    ],
-    install_requires=[
-        'numpy',
-        'scipy',
-        'mpmath',
-        'matplotlib',
-        'cython',
-        'jax',
-        'lineax',
-        'flax'
-    ],
-    ext_modules=extensions,
-    zip_safe=False,
-    packages=['propagators', 'rwp', 'uwa', 'transforms', 'transforms.fcc_fourier', 'experimental'],
-    package_data={
-        '': ['*.pyx']
-    },
-    keywords=["wave propagation", "parabolic equation", "troposphere", "underwater acoustics", "diffraction", "refraction"]
-)
+setup(ext_modules=build_extensions())
